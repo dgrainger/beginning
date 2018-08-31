2dd8e42a2dbe417864950b81729817cad71d1e85
Darrell Grainger <dgrainge@thoughtworks.com>
2018-08-31 19:30:52 -0400

    Change code to be an actual test with a pass/fail result

--- src/com/darrell/Example1.java
+++ src/com/darrell/Example1.java
@@ -3,8 +3,12 @@ package com.darrell;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
 
 public class Example1 {
     WebDriver driver;
@@ -17,7 +21,10 @@ public class Example1 {
     @Test
     public void example_test() {
         driver.get("https://www.google.com");
-        System.out.println(driver.getTitle());
+        WebElement input = driver.findElement(By.cssSelector("[name='q']"));
+        input.sendKeys("Selenium\n");
+        String title = driver.getTitle();
+        assertEquals("Selenium - Google Search", title);
     }
 
     @After

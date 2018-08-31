65afd6609cd0eb0f58c1440c3618ce768dfb5487
Darrell Grainger <dgrainge@thoughtworks.com>
2018-08-31 19:06:03 -0400

    Add .gitignore and first example of Selenium code

--- src/com/darrell/Example1.java
+++ src/com/darrell/Example1.java
@@ -1,10 +1,15 @@
 package com.darrell;
 
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Example1 {
     @Test
     public void example_test() {
-
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+        driver.quit();
     }
 }

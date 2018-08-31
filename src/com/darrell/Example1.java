35ed2ee54111ff511ea5998b1a6a92fa17579cd0
Darrell Grainger <dgrainge@thoughtworks.com>
2018-08-31 19:09:22 -0400

    Improved version of previous example_test()

--- src/com/darrell/Example1.java
+++ src/com/darrell/Example1.java
@@ -1,15 +1,27 @@
 package com.darrell;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Example1 {
+    WebDriver driver;
+
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+
     @Test
     public void example_test() {
-        WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
         System.out.println(driver.getTitle());
+    }
+
+    @After
+    public void tearDown() {
         driver.quit();
     }
 }
